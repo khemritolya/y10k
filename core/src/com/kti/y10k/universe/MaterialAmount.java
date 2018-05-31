b53563dc2d2ecd4df4abfbd50db27b8bbaef4f9e
Khemri Tolya <khemritolya@gmail.com>
2018-05-31 08:09:24 -0400

    .

--- core/src/com/kti/y10k/universe/MaterialAmount.java
+++ core/src/com/kti/y10k/universe/MaterialAmount.java
@@ -5,6 +5,13 @@ public class MaterialAmount {
     public Material m;
 
     public enum Material {
+        Iron(10, 4, null),
+        Titanium(6, 10, null),
+        Aluminium(8, 6, null),
+        Radioactives(15, 10, null),
+        Hydrogen(1, 50, null),
+        Gas(2, 70, new MaterialAmount[] {
+                new MaterialAmount(Material.Hydrogen, 1)}),
         Titanite(20, 10, new MaterialAmount[] {
                 new MaterialAmount(Material.Titanium, 6),
                 new MaterialAmount(Material.Radioactives, 1)}),
@@ -20,14 +27,7 @@ public class MaterialAmount {
         Trikite(50, 130, new MaterialAmount[] {
                 new MaterialAmount(Material.Aluminium, 6),
                 new MaterialAmount(Material.Iron, 1),
-                new MaterialAmount(Material.Gas, 1)}),
-        Gas(2, 70, new MaterialAmount[] {
-                new MaterialAmount(Material.Hydrogen, 1)}),
-        Iron(10, 4, null),
-        Titanium(6, 10, null),
-        Aluminium(8, 6, null),
-        Radioactives(15, 10, null),
-        Hydrogen(1, 50, null);
+                new MaterialAmount(Material.Gas, 1)});
 
         public final int massPerUnit;
         public final int volumePerUnit;
